37d28a529a9dd5131eda351e55500c0d171bbcea
AshleyEM <35859215+AshleyEM@users.noreply.github.com>
2018-03-26 12:38:47 -0700

    Add files via upload

---
+++ EndActivity.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace app_test
+{
+    [Activity(Label = "EndActivity")]
+    public class EndActivity : Activity // END SCREEN
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.End);
+
+            Button buttonMain = FindViewById<Button>(Resource.Id.buttonMain);
+
+            buttonMain.Click += delegate {
+                var intentMain = new Intent(this, typeof(MainActivity));
+                StartActivity(intentMain);
+            };
+        }
+    }
+}
