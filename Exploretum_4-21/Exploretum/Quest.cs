b4e9269b8e5dcf272ccfaf72f0d4dcc71d7679bf
AshleyEM <35859215+AshleyEM@users.noreply.github.com>
2018-04-22 23:24:24 -0700

    Add files via upload

---
+++ Exploretum_4-21/Exploretum/Quest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Exploretum
+{
+    public class Quest
+    {
+        // information for each quest
+        private static string[][] questList = {
+            new string[] {"Moth and Magnolia",  
+                          "47.591481", // lat  
+                          "-122.083228", // long 
+                          "[spirit's story]", 
+                          "[spirit1 thanks user]", 
+                         },
+            new string[] {"quest 2",
+                          "47.591673 ", 
+                          "-122.083251",
+                          "[spirit's story]",
+                          "[spirit2 thanks user]",  
+                         },
+ 
+            // ...
+        };
+        // sprite images for each quest
+        private static int[] spriteImgs = {
+            Resource.Drawable.leopard_moth_sprite,
+            Resource.Drawable.magnolia_sprite,
+        };
+        // hot-cold meter images
+        public static int[] scopeFrames = {
+            Resource.Drawable.hotcold1,
+            Resource.Drawable.hotcold2,
+            Resource.Drawable.hotcold3,
+            Resource.Drawable.hotcold4,
+            Resource.Drawable.hotcold5,
+            Resource.Drawable.hotcold6,
+            Resource.Drawable.hotcold7,
+            Resource.Drawable.hotcold8,
+            Resource.Drawable.hotcold9,
+            Resource.Drawable.hotcold10,
+            Resource.Drawable.hotcold11
+        };
+        // return sprite image for Resource.Drawable
+        public static int GetSpriteImg(int questNumber){
+            return spriteImgs[questNumber];
+        }
+        // return the coordinates { latitude, longitude } of given quest
+        public static string[] QuestCoords(int questNumber){
+            string[] coords = new string[2];
+            coords[0] = questList[questNumber][1];
+            coords[1] = questList[questNumber][2];
+            return coords;
+        }
+        // return the number of total quests
+        public static int QuestLimit()
+        {
+            return questList.Length-1;
+        }
+        // return quest name based on questNumber
+        public static string GetQuestName(int questNumber)
+        {
+            return questList[questNumber][0];
+        }
+        public static string GetQuestStory(int questNumber)
+        {
+            return questList[questNumber][3];
+        }
+        public static string GetQuestCompleted(int questNumber)
+        {
+            return questList[questNumber][4];
+        }
+        // for debugging
+        public void printQuests()
+        {
+            foreach (string[] s in questList)
+            {
+                Console.WriteLine(s[0]);
+            }
+        }
+
+    }
+}
---
+++ Exploretum_4-21/Exploretum/QuestActivity.cs
@@ -0,0 +1,201 @@
+﻿
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Content.PM;
+using Android.Locations;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android;
+
+namespace Exploretum  // ALL QUEST SCREENS
+{
+    [Activity(Label = "Quest")]
+
+    public class QuestActivity : Activity, ILocationListener
+    {
+        int questNumber;
+        const long ONE_SECOND = 500;
+        const int RequestLocationId = 0;
+        LocationManager locationManager;
+        TextView longitude;
+        TextView latitude;
+        TextView provider;
+        TextView distanceText;
+        ImageView scope;
+        ImageView spritePlaceholder;
+        int[] scope_frames = Quest.scopeFrames;
+
+        // permission to access course/fine location
+        readonly string[] PermissionsLocation =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Quest);
+
+            // text views for coordinates
+            latitude = FindViewById<TextView>(Resource.Id.latitude);
+            longitude = FindViewById<TextView>(Resource.Id.longitude);
+            provider = FindViewById<TextView>(Resource.Id.provider);
+
+            // get location manager
+            locationManager = (LocationManager)GetSystemService(LocationService);
+            
+            latitude.Text = "Getting GPS coordinates ...";
+            longitude.Text = " ";
+            provider.Text = " ";
+
+            RequestPermissions(PermissionsLocation, RequestLocationId);
+
+
+            // ---MY CODE----
+            questNumber = Intent.GetIntExtra("questNumber", 0);
+            // if end of quest list
+            if (questNumber == Quest.QuestLimit() + 1)
+            {
+                // Go to end screen
+                var intentEnd = new Intent(this, typeof(EndActivity));
+                StartActivity(intentEnd);
+            }
+            else
+            {
+                // read from JSON data file
+                StreamReader stream = new StreamReader(Assets.Open("Data.json"));
+                
+                // display quest info 
+                TextView questName = FindViewById<TextView>(Resource.Id.questName);
+                questName.Text = Quest.GetQuestName(questNumber);
+
+                //display spectrescope as 'cold' until GPS coordinates are loaded
+                scope = FindViewById<ImageView>(Resource.Id.scope);
+                scope.SetImageResource(scope_frames[scope_frames.Length-1]);
+
+                // (debugging) see distance between quest location/current location
+                distanceText = FindViewById<TextView>(Resource.Id.distance);
+                // For debugging only
+                Button button = FindViewById<Button>(Resource.Id.button1);
+                button.Click += delegate
+                {
+                    // Start Quest Activity
+                    var intentStartQuest = new Intent(this, typeof(CompleteQuestActivity));
+                    intentStartQuest.PutExtra("questNumber", questNumber); // passing variable to QuestActivity
+                    StartActivity(intentStartQuest); // passing variable to QuestActivity
+                };
+            }
+
+
+
+            // -/---MY CODE----
+        }
+
+
+        // ------GPS SAMPLE CODE (including comments)----
+        public override async void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            switch (requestCode)
+            {
+                // If this is for my app and not someone else's (if this is my request ID):
+                case RequestLocationId:
+                    {
+                        // If permission was granted to my app:
+                        if (grantResults[0] == Permission.Granted)
+                        {
+                            locationManager.RequestLocationUpdates(LocationManager.GpsProvider, ONE_SECOND, 1, this);
+                        }
+                        else
+                            // If permission wasn't granted, then pop up a toast to say so
+                            // *don't* try to request location updates (that would cause a security
+                            // exception):
+                            Toast.MakeText(this, "Location permission was denied!", ToastLength.Short).Show();
+                    }
+                    break;
+            }
+
+            // Call the superclass's method as well:
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+// -------------- GPS CONTROL ------------
+        public void OnLocationChanged(Location location)
+        {
+            latitude.Text = Resources.GetString(Resource.String.latitude_string, location.Latitude);
+            longitude.Text = Resources.GetString(Resource.String.longitude_string, location.Longitude);
+            provider.Text = Resources.GetString(Resource.String.provider_string, location.Provider);
+
+            double currentLat = location.Latitude;
+            double currentLng = location.Longitude;
+            float[] distancetmp = new float[1];
+            double questLat = Convert.ToDouble(Quest.QuestCoords(questNumber)[0]);
+            double questLng = Convert.ToDouble(Quest.QuestCoords(questNumber)[1]);
+            Location.DistanceBetween(currentLat, currentLng, questLat, questLng, distancetmp);
+            double distance = distancetmp[0];
+
+            // display distance (double) from objective
+            distanceText.Text = "Distance: " + distance.ToString() + " meters";
+
+            // Show sprite based on quest number
+            spritePlaceholder = FindViewById<ImageView>(Resource.Id.sprite);
+            int spriteImg = Quest.GetSpriteImg(questNumber);
+            spritePlaceholder.SetImageResource(spriteImg);
+
+            int distanceRounded = Convert.ToInt32(distance); // round current distance 
+            const int RANGE = 5; // How spread out distances are (smaller number = more frames). Multiplied by index of spectrescope frames.  
+            int indexOfFrameToShow = distanceRounded / RANGE;
+            //                          {img1,  img2,   img3 ... }    
+            //                   HOT     0m      5m      10m     15m     20m   COLD
+            //                          0*5     1*5     2*5     3*5     4*5 
+            //                      index*RANGE                           
+
+            // RANGE * index of image to show = current distance from objective  
+            // index of image to show = current distance from objective / RANGE 
+            // EXAMPLE: 
+            //    current distance = 12
+            //    current distance / RANGE = index of image to show 
+            //    12 / 4 = index of image to show 
+            //    = scope_frames[3] 
+            scope = FindViewById<ImageView>(Resource.Id.scope);
+            
+            if(indexOfFrameToShow < scope_frames.Length){
+                scope.SetImageResource(scope_frames[indexOfFrameToShow]);
+            }else{
+                // if distance is beyond the scope, just show the 'cold' (last) frame by default
+                scope.SetImageResource(scope_frames[scope_frames.Length -1]);
+            }
+        
+
+            // if distance is less than 4, start quest activity
+
+            
+            
+            
+        }
+// -------------- /GPS CONTROL ------------
+
+
+
+
+        public void OnProviderDisabled(string provider)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+        }
+    }
+}
