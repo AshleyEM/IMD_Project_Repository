04a6ce42b277fdaf5b6a910004a0e77eea2eda68
AshleyEM <35859215+AshleyEM@users.noreply.github.com>
2018-04-29 15:54:45 -0700

    Add files via upload

---
+++ Exploretum_4-29/Exploretum/StoryActivity.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json.Linq;
+
+namespace Exploretum
+{
+    [Activity(Label = "StoryActivity")]
+    public class StoryActivity : Activity // END SCREEN
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Story);
+            int questNumber = Intent.GetIntExtra("questNumber", 0);
+            int stopNumber = Intent.GetIntExtra("stopNumber", 0);
+
+            // turn json file into string
+            string json;
+            using (StreamReader r = new StreamReader(Assets.Open("GameData.json")))
+            {
+                json = r.ReadToEnd();
+            }
+            JObject obj = JObject.Parse(json);
+            int questsLength = (int)obj.SelectToken("Game.Quests").Count() - 1;
+            int stopsLength = (int)obj.SelectToken("Game.Quests["+questNumber+"].stops").Count() - 1; 
+
+            // if end of quest list
+            if (questNumber == 5) // if reached the end of all quests
+            {
+                // Go to end screen
+                var intentEnd = new Intent(this, typeof(EndActivity));
+                StartActivity(intentEnd);
+            }
+            else
+            {
+                // quest/stop info from JSON file
+                string q_name = (string)obj.SelectToken("Game.Quests["+ questNumber +"].name");   
+                string s_name = (string)obj.SelectToken("Game.Quests["+ questNumber +"].stops["+stopNumber+"].stopname");
+                string s_type = (string)obj.SelectToken("Game.Quests["+ questNumber +"].stops["+stopNumber+"].stoptype");
+                TextView questName = FindViewById<TextView>(Resource.Id.questName);
+                TextView stopName = FindViewById<TextView>(Resource.Id.stopName);
+                questName.Text = q_name;
+                stopName.Text = s_name + " " + s_type + "STOP NUMBER: "+stopNumber;
+
+                Button button = FindViewById<Button>(Resource.Id.button1);
+                // MOVING TO NEXT STOP
+                button.Click += delegate
+                {   
+                    string next_stop_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+(stopNumber+1)+"].stoptype"); 
+                    Intent intent;
+                    // if reached the end of all stops, increment quest number and reset stop number
+                    if (next_stop_type != "story" && next_stop_type != "checkpoint"){
+                        questNumber = questNumber + 1;
+                        stopNumber = 0; 
+                        next_stop_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].stoptype");
+                        stopNumber = -1; // becase the code down below will increment 0 + 1, and will skip the first [0] stop of next quest
+                    }
+                    if (next_stop_type == "story")
+                    {
+                        intent = new Intent(this, typeof(StoryActivity));
+                        intent.PutExtra("questNumber", questNumber);
+                        intent.PutExtra("stopNumber", stopNumber + 1);
+                        StartActivity(intent);
+                    }
+                    else if (next_stop_type == "checkpoint")
+                    {
+                        intent = new Intent(this, typeof(CheckpointActivity));
+                        intent.PutExtra("questNumber", questNumber);
+                        intent.PutExtra("stopNumber", stopNumber + 1);
+                        StartActivity(intent);
+                    }
+                };
+            }
+        }
+    }
+}
