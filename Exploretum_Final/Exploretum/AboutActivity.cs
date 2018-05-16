3f6bce57c3b9ae049c0d3f71f26dccf93d8953e4
AshleyEM <35859215+AshleyEM@users.noreply.github.com>
2018-05-16 13:44:59 -0700

    Add files via upload

---
+++ Exploretum_Final/Exploretum/AboutActivity.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Exploretum
+{
+    [Activity(Label = "AboutActivity")]
+    public class AboutActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.About);
+            // Create your application here
+        }
+    }
+}
