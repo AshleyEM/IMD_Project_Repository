f8d55fc65e587577b4433a31eb13b1714044347a
AshleyEM <35859215+AshleyEM@users.noreply.github.com>
2018-05-16 13:58:07 -0700

    Update MainActivity.cs

--- Exploretum_Final/Exploretum/MainActivity.cs
+++ Exploretum_Final/Exploretum/MainActivity.cs
@@ -20,7 +20,6 @@ namespace Exploretum // FIRST SCREEN USER SEES
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
-	    RequestWindowFeature(WindowFeatures.NoTitle);
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
             
@@ -32,8 +31,8 @@ namespace Exploretum // FIRST SCREEN USER SEES
             JObject obj = JObject.Parse(json);
             string first_s_type = (string)obj.SelectToken("Game.Quests[0].stops[0].stoptype");
 
-			Button startMoth = FindViewById<Button>(Resource.Id.Moth);
-			Button startPine = FindViewById<Button>(Resource.Id.Pine);
+	    Button startMoth = FindViewById<Button>(Resource.Id.Moth);
+	    Button startPine = FindViewById<Button>(Resource.Id.Pine);
             Button startHummingbird = FindViewById<Button>(Resource.Id.Hummingbird);
 
             startMoth.Click += delegate
@@ -41,12 +40,12 @@ namespace Exploretum // FIRST SCREEN USER SEES
                 if (first_s_type == "story"){
                     var intentStart = new Intent(this, typeof(StoryActivity));
                     intentStart.PutExtra("questNumber", (int)0);
-                    intentStart.PutExtra("stopNumber", (int)0);
+                    intentStart.PutExtra("stopNumber", (int)SavedState.getStateOf(0));
                     StartActivity(intentStart);
                 }else if (first_s_type == "checkpoint"){
                     var intentStart = new Intent(this, typeof(CheckpointActivity));
                     intentStart.PutExtra("questNumber", (int)0);
-                    intentStart.PutExtra("stopNumber", (int)0);
+                    intentStart.PutExtra("stopNumber", (int)SavedState.getStateOf(0));
                     StartActivity(intentStart);
                 }
             };
@@ -55,12 +54,12 @@ namespace Exploretum // FIRST SCREEN USER SEES
                 if (first_s_type == "story"){
                     var intentStart = new Intent(this, typeof(StoryActivity));
                     intentStart.PutExtra("questNumber", (int)1);
-                    intentStart.PutExtra("stopNumber", (int)0);
+                    intentStart.PutExtra("stopNumber", (int)SavedState.getStateOf(1));
                     StartActivity(intentStart);
                 }else if (first_s_type == "checkpoint"){
                     var intentStart = new Intent(this, typeof(CheckpointActivity));
                     intentStart.PutExtra("questNumber", (int)1);
-                    intentStart.PutExtra("stopNumber", (int)0);
+                    intentStart.PutExtra("stopNumber", (int)SavedState.getStateOf(1));
                     StartActivity(intentStart);
                 }
             };
@@ -69,12 +68,12 @@ namespace Exploretum // FIRST SCREEN USER SEES
                 if (first_s_type == "story"){
                     var intentStart = new Intent(this, typeof(StoryActivity));
                     intentStart.PutExtra("questNumber", (int)2);
-                    intentStart.PutExtra("stopNumber", (int)0);
+                    intentStart.PutExtra("stopNumber", (int)SavedState.getStateOf(2));
                     StartActivity(intentStart);
                 }else if (first_s_type == "checkpoint"){
                     var intentStart = new Intent(this, typeof(CheckpointActivity));
                     intentStart.PutExtra("questNumber", (int)2);
-                    intentStart.PutExtra("stopNumber", (int)0);
+                    intentStart.PutExtra("stopNumber", (int)SavedState.getStateOf(2));
                     StartActivity(intentStart);
                 }
             };
