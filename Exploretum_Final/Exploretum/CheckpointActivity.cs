9bcf01aa28512fa619fbb8f4c1471251a9f487f5
AshleyEM <35859215+AshleyEM@users.noreply.github.com>
2018-05-20 18:27:17 -0700

    Update CheckpointActivity.cs

--- Exploretum_Final/Exploretum/CheckpointActivity.cs
+++ Exploretum_Final/Exploretum/CheckpointActivity.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -52,11 +52,7 @@ namespace Exploretum  // ALL QUEST SCREENS
             Resource.Drawable.hotcold8,
             Resource.Drawable.hotcold9,
             Resource.Drawable.hotcold10,
-            Resource.Drawable.hotcold11,
-            Resource.Drawable.hotcold12,
-            Resource.Drawable.hotcold13,
-            Resource.Drawable.hotcold14,
-            Resource.Drawable.hotcold15
+            Resource.Drawable.hotcold11
         };
 
         // permission to access course/fine location
@@ -67,16 +63,17 @@ namespace Exploretum  // ALL QUEST SCREENS
         };
 
         protected override void OnCreate(Bundle savedInstanceState)
-        {
+        {           
+            Console.WriteLine(">>>>>>>>>>>>>> CHECKPOINT ACTIVITY");
             RequestWindowFeature(WindowFeatures.NoTitle);
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Checkpoint);
             questNumber = Intent.GetIntExtra("questNumber", 0);
             stopNumber = Intent.GetIntExtra("stopNumber", 0);
-            // Save current stop number for this quest
-            SavedState.setStateOf(questNumber, stopNumber);
+            // Save current stop number for this quest (go back to prev story activty becase GPS breaks when reloading)
+            SavedState.setStateOf(questNumber, stopNumber-1);
             RequestPermissions(PermissionsLocation, RequestLocationId);
-
+                     
             // read from JSON data file
             using (StreamReader r = new StreamReader(Assets.Open("GameData.json")))
             {
@@ -85,140 +82,129 @@ namespace Exploretum  // ALL QUEST SCREENS
             obj = JObject.Parse(json);
             questsLength = (int)obj.SelectToken("Game.Quests").Count() - 1;
             stopsLength = (int)obj.SelectToken("Game.Quests["+questNumber+"].stops").Count()-1; 
+               
+            // quest/stop info from JSON file
+            string q_name = (string)obj.SelectToken("Game.Quests["+questNumber+"].name");
+            string s_name = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].stopname");
+            string s_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].stoptype");  
+            string plant_name = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].plant"); 
+            string common_name = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].common");     
+            string bgImgName = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].backgroundImg"); 
+            string spriteImgName = (string)obj.SelectToken("Game.Quests["+questNumber+"].spriteImg");   
+            string factText = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].fact");  
+            string hintText = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].hint");
+            string next_stop_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+(stopNumber+1)+"].stoptype");
 
-            // if end of quest list
-            if (questNumber == 5) // if reached the end of all quests
-            {
-                // Go to end screen
-                var intentEnd = new Intent(this, typeof(EndActivity));
-                StartActivity(intentEnd);
-            }
-            else
-            {        
-                // quest/stop info from JSON file
-                string q_name = (string)obj.SelectToken("Game.Quests["+questNumber+"].name");
-                string s_name = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].stopname");
-                string s_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].stoptype");  
-                string plant_name = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].plant"); 
-                string common_name = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].common");     
-                string bgImgName = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].backgroundImg"); 
-                string spriteImgName = (string)obj.SelectToken("Game.Quests["+questNumber+"].spriteImg");   
-                string factText = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].fact");  
-                string hintText = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].hint");
-                string next_stop_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+(stopNumber+1)+"].stoptype");
+			// hide 'PLANT FOUND!' button
+			foundButton = FindViewById<Button>(Resource.Id.found);
+			foundButton.Visibility = ViewStates.Invisible;
 
-				// hide 'PLANT FOUND!' button
-				foundButton = FindViewById<Button>(Resource.Id.found);
-				foundButton.Visibility = ViewStates.Invisible;
+			debugNextButton = FindViewById<Button>(Resource.Id.next);
+            debugNextButton.Click += delegate {
+			
+                Intent Debugintent;
+                // if reached the end of all stops, increment quest number and reset stop number
+                if (next_stop_type != "story" && next_stop_type != "checkpoint"){
+                    questNumber = questNumber + 1;
+                    stopNumber = 0; 
+                    next_stop_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].stoptype");
+                    stopNumber = -1; // becase the code down below will increment 0 + 1, and will skip the first [0] stop of next quest
+                }
+                if (next_stop_type == "story")
+                {
+                    Debugintent = new Intent(this, typeof(StoryActivity));
+                    Debugintent.PutExtra("questNumber", questNumber);
+                    Debugintent.PutExtra("stopNumber", stopNumber + 1);
+                    StartActivity(Debugintent);
+                }
+                else if (next_stop_type == "checkpoint")
+                {
+                    Debugintent = new Intent(this, typeof(CheckpointActivity));
+                    Debugintent.PutExtra("questNumber", questNumber);
+                    Debugintent.PutExtra("stopNumber", stopNumber + 1);
+                    StartActivity(Debugintent);
+                }
+			};
+            // Show sprite based on quest number
+            spriteImgView = FindViewById<ImageView>(Resource.Id.sprite);
+            int spriteImgId = Resources.GetIdentifier(spriteImgName, "drawable", PackageName);
+            spriteImgView.SetImageResource(spriteImgId);
+            backgroundImgView = FindViewById<ImageView>(Resource.Id.background);
+            int bgImgId = Resources.GetIdentifier(bgImgName, "drawable", PackageName);
+            backgroundImgView.SetImageResource(bgImgId);
 
-				debugNextButton = FindViewById<Button>(Resource.Id.next);
-                debugNextButton.Click += delegate {
-				
-                    Intent Debugintent;
-                    // if reached the end of all stops, increment quest number and reset stop number
-                    if (next_stop_type != "story" && next_stop_type != "checkpoint"){
-                        questNumber = questNumber + 1;
-                        stopNumber = 0; 
-                        next_stop_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].stoptype");
-                        stopNumber = -1; // becase the code down below will increment 0 + 1, and will skip the first [0] stop of next quest
-                    }
-                    if (next_stop_type == "story")
-                    {
-                        Debugintent = new Intent(this, typeof(StoryActivity));
-                        Debugintent.PutExtra("questNumber", questNumber);
-                        Debugintent.PutExtra("stopNumber", stopNumber + 1);
-                        StartActivity(Debugintent);
-                    }
-                    else if (next_stop_type == "checkpoint")
-                    {
-                        Debugintent = new Intent(this, typeof(CheckpointActivity));
-                        Debugintent.PutExtra("questNumber", questNumber);
-                        Debugintent.PutExtra("stopNumber", stopNumber + 1);
-                        StartActivity(Debugintent);
-                    }
-				};
-                // Show sprite based on quest number
-                spriteImgView = FindViewById<ImageView>(Resource.Id.sprite);
-                int spriteImgId = Resources.GetIdentifier(spriteImgName, "drawable", PackageName);
-                spriteImgView.SetImageResource(spriteImgId);
-                backgroundImgView = FindViewById<ImageView>(Resource.Id.background);
-                int bgImgId = Resources.GetIdentifier(bgImgName, "drawable", PackageName);
-                backgroundImgView.SetImageResource(bgImgId);
+            ImageButton homeButton = FindViewById<ImageButton>(Resource.Id.home);
+            homeButton.Click += delegate {
+                var intent = new Intent(this, typeof(MainActivity));
+				StartActivity(intent);
+			};
 
-                ImageButton homeButton = FindViewById<ImageButton>(Resource.Id.home);
-                homeButton.Click += delegate {
-                    var intent = new Intent(this, typeof(MainActivity));
-					StartActivity(intent);
-				};
-
-				// display info/hint on button click
-				hintButton = FindViewById<ImageButton>(Resource.Id.hint);
-                factButton = FindViewById<ImageButton>(Resource.Id.fact);
-                hintButton.Click += delegate {
-					AlertDialog.Builder alert = new AlertDialog.Builder(this);
-					AlertDialog popup = alert.Create();
-					popup.SetTitle("Hint");
-					popup.SetMessage(hintText);
-					popup.SetButton("Ok", (c, ev) => { });
-					popup.Show();
-				};   
-                factButton.Click += delegate {
-                    AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                    AlertDialog popup = alert.Create();
-                    popup.SetTitle("Fact");
-                    popup.SetMessage(factText);
-                    popup.SetButton("Ok", (c, ev) => { });
-                    popup.Show();
-                };  
-                    
-                // display lat/long
-                latitude = FindViewById<TextView>(Resource.Id.latitude);
-                longitude = FindViewById<TextView>(Resource.Id.longitude);              
-
-                // get location manager
-                locationManager = (LocationManager)GetSystemService(LocationService);
+			// display info/hint on button click
+			hintButton = FindViewById<ImageButton>(Resource.Id.hint);
+            factButton = FindViewById<ImageButton>(Resource.Id.fact);
+            hintButton.Click += delegate {
+				AlertDialog.Builder alert = new AlertDialog.Builder(this);
+				AlertDialog popup = alert.Create();
+				popup.SetTitle("Hint");
+				popup.SetMessage(hintText);
+				popup.SetButton("Ok", (c, ev) => { });
+				popup.Show();
+			};   
+            factButton.Click += delegate {
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                AlertDialog popup = alert.Create();
+                popup.SetTitle("Fact");
+                popup.SetMessage(factText);
+                popup.SetButton("Ok", (c, ev) => { });
+                popup.Show();
+            };  
                 
-                latitude.Text = "Loading GPS...";
-                longitude.Text = " ";
-                //display spectrescope as 'cold' until GPS coordinates are loaded
-                scope = FindViewById<ImageView>(Resource.Id.scope);
-                scope.SetImageResource(scope_frames[scope_frames.Length-1]);
+            // display lat/long
+            latitude = FindViewById<TextView>(Resource.Id.latitude);
+            longitude = FindViewById<TextView>(Resource.Id.longitude);              
 
-                // (debugging) see distance between quest location/current location
-                distanceText = FindViewById<TextView>(Resource.Id.distance);
+            // get location manager
+            locationManager = (LocationManager)GetSystemService(LocationService);
+            
+            latitude.Text = "Loading GPS...";
+            longitude.Text = " ";
+            //display spectrescope as 'cold' until GPS coordinates are loaded
+            scope = FindViewById<ImageView>(Resource.Id.scope);
+            scope.SetImageResource(scope_frames[scope_frames.Length-1]);
 
-                // Go to next stop
-                foundButton.Click += delegate
-                {   
-                    Intent intent;
-                    // if reached the end of all stops, increment quest number and reset stop number
-                    if (next_stop_type != "story" && next_stop_type != "checkpoint"){
-                        questNumber = questNumber + 1;
-                        stopNumber = 0; 
-                        next_stop_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].stoptype");
-                        stopNumber = -1; // becase the code down below will increment 0 + 1, and will skip the first [0] stop of next quest
-                    }
-                    if (next_stop_type == "story")
-                    {
-                        intent = new Intent(this, typeof(StoryActivity));
-                        intent.PutExtra("questNumber", questNumber);
-                        intent.PutExtra("stopNumber", stopNumber + 1);
-                        StartActivity(intent);
-                    }
-                    else if (next_stop_type == "checkpoint")
-                    {
-                        intent = new Intent(this, typeof(CheckpointActivity));
-                        intent.PutExtra("questNumber", questNumber);
-                        intent.PutExtra("stopNumber", stopNumber + 1);
-                        StartActivity(intent);
-                    }
-                };
-            }
+            // (debugging) see distance between quest location/current location
+            distanceText = FindViewById<TextView>(Resource.Id.distance);
 
+            // Go to next stop
+            foundButton.Click += delegate
+            {   
+                Intent intent;
+                // if reached the end of all stops, increment quest number and reset stop number
+                if (next_stop_type != "story" && next_stop_type != "checkpoint"){
+                    questNumber = questNumber + 1;
+                    stopNumber = 0; 
+                    next_stop_type = (string)obj.SelectToken("Game.Quests["+questNumber+"].stops["+stopNumber+"].stoptype");
+                    stopNumber = -1; // becase the code down below will increment 0 + 1, and will skip the first [0] stop of next quest
+                }
+                if (next_stop_type == "story")
+                {
+                    intent = new Intent(this, typeof(StoryActivity));
+                    intent.PutExtra("questNumber", questNumber);
+                    intent.PutExtra("stopNumber", stopNumber + 1);
+                    StartActivity(intent);
+                }
+                else if (next_stop_type == "checkpoint")
+                {
+                    intent = new Intent(this, typeof(CheckpointActivity));
+                    intent.PutExtra("questNumber", questNumber);
+                    intent.PutExtra("stopNumber", stopNumber + 1);
+                    StartActivity(intent);
+                }
+            };
+                       
         }
 
-
-
+     
         // ------GPS SAMPLE CODE (including comments)----
         public override async void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
         {
@@ -268,11 +254,11 @@ namespace Exploretum  // ALL QUEST SCREENS
             distanceText.Text = "Distance: " + distance.ToString() + " meters";
 
             int distanceRounded = Convert.ToInt32(distance); // round current distance 
-            const int RANGE = 8; // How spread out distances are (smaller number = more frames). Multiplied by index of spectrescope frames.  
+            const int RANGE = 10; // How spread out distances are (smaller number = more frames). Multiplied by index of spectrescope frames.  
             int indexOfFrameToShow = distanceRounded / RANGE;
             //                          {img1,  img2,   img3 ... }    
-            //                   HOT     0m      8m      16m     24m     32m   COLD
-            //                          0*8     1*8     2*8     3*8     4*8 
+            //                   HOT     0m      10m      20m       COLD
+            //                          0*10     1*10     2*10    
             //                      index*RANGE                           
 
             // RANGE * index of image to show = current distance from objective  
@@ -291,7 +277,7 @@ namespace Exploretum  // ALL QUEST SCREENS
                 scope.SetImageResource(scope_frames[scope_frames.Length -1]);
             }
 			// if close to coordinates, reveal PLANT FOUND! button
-			if (distance <= 20)
+			if (distance <= 15)
 			{
 				foundButton.Visibility = ViewStates.Visible;
 			}
@@ -302,9 +288,6 @@ namespace Exploretum  // ALL QUEST SCREENS
             
         }
 // -------------- /GPS CONTROL ------------
-
-
-
 
         public void OnProviderDisabled(string provider)
         {
